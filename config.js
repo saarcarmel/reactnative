cebd36a754bc891039cc0d38534adfd3b064e370
saarcarmel <34777512+saarcarmel@users.noreply.github.com>
2018-01-16 02:43:41 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+  clientID: 145075239530071,
+  clientSecret: 31e380fa1dde23025e3ec429cb787ff4,
+  callbackURL: 'http://localhost:3000/auth/facebook/callback',
+  profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+};
+
+export const google = {
+  clientID: 559784047726-44kie0te4jhu2uq95cpas2kktksl6r2r.apps.googleusercontent.com,
+  clientSecret: 3UzWGW-hLqxCpC6zFm2TCirL,
+  callbackURL: 'http://localhost:3000/auth/google/callback',
+};
